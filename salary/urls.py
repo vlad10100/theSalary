10aeb83b084edce5ba4ebc9c7087e93a2dff8e0b
vlad10100 <vbucaralde@gmail.com>
2022-01-31 03:18:49 +0800

    new base

--- salary/urls.py
+++ salary/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 
-from .views import HomePageView, SalaryPostDetail, SalaryPage
+from .views import HomePageView, SalaryPostDetail, SalaryPage, IndustryListPage
 
 app_name = 'salary'
 
 urlpatterns = [
     path('home/', HomePageView.as_view(), name='home_page'),
     path('salary-post/<int:pk>/', SalaryPostDetail.as_view(), name='salary_post_detail'),
-    path('salary-page/', SalaryPage.as_view(), name='salary_page')    
+    path('salary-page/', SalaryPage.as_view(), name='salary_page'),
+
+    path('industry/', IndustryListPage.as_view(), name='industry_page'),
 ]
--- salary/views.py
+++ salary/views.py
@@ -12,6 +12,7 @@ from django.views.generic import (CreateView, DetailView, ListView,
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from account.models import UserProfile
+from .models import Industry
 
 
 
@@ -23,6 +24,7 @@ class HomePageView(ListView):                                                   
     def get_context_data(self, **kwargs):
         id_list = UserProfile.objects.values('id')
         # id_list -->>>  <QuerySet [{'id': 4}, {'id': 7}, {'id': 3}, {'id': 6}, {'id': 1}, {'id': 2}, {'id': 5}]>
+        print(id_list)
         query = {'id':self.request.user.id}
         context = super().get_context_data(**kwargs)
         context['id_list'] = id_list
@@ -35,75 +37,15 @@ class SalaryPostDetail(DetailView):
     context_object_name = 'post'
     queryset = UserProfile.objects.all()
 
+
+
 class SalaryPage(ListView):
     template_name = 'salary_blog/salary_page.html'
     queryset = UserProfile.objects.all()
+    # No context yet...
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class CreateSalaryBlog(LoginRequiredMixin, UserPassesTestMixin, CreateView):                 # create
-#     template_name = 'salary_blog/create_post.html'
-#     form_class = SalaryBlog_Form
-#     raise_exception = True
-#     queryset = SalaryBlog.objects.all()
-#     success_url = reverse_lazy('salary:home_page')
-
-#     def test_func(self):
-#         if self.request.user.id in SalaryBlog.objects.values('user_id'):
-#             print(self.request.user)
-#             return True
-#         else:
-#             return False
-        
-
-#     def form_valid(self, form):
-#         form.instance.user = self.request.user
-#         return super(CreateSalaryBlog, self).form_valid(form)
-
-
-# class UpdateSalaryBlog(LoginRequiredMixin, UpdateView):                 # update
-#     template_name = 'salary_blog/update_post.html'
-#     form_class = SalaryBlog_Form
-#     queryset = SalaryBlog.objects.all()
-#     success_url = reverse_lazy('salary:home_page')
-
-
-
-# class DeleteSalaryBlog(LoginRequiredMixin, DeleteView):                 # confirm deletion
-#     template_name = 'salary_blog/delete_post.html'
-#     queryset = SalaryBlog.objects.all()
-#     success_url = reverse_lazy('salary:home_page')
-
-
-
-# class SalaryBlogDetail(DetailView):                                     # specific blog detail
-#     template_name = 'salary_blog/post_detail.html'
-#     queryset = SalaryBlog.objects.all()
-#     context_object_name = 'list'
-
-
-
-# #                                   INDUSTRY 
+class IndustryListPage(ListView):
+    template_name = 'industry/industry_page.html'
+    queryset = Industry.objects.all()
+    context_object_name = 'industries'
